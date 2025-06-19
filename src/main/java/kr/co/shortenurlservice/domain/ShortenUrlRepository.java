f0452742d6abea9edf203a93dfa11f533a6a06ac
qwe5507 <apvmffkdls@gmail.com>
2025-06-19 17:24:06 +0900

    캐싱 활용 성능 개성 - 메모리

--- src/main/java/kr/co/shortenurlservice/domain/ShortenUrlRepository.java
+++ src/main/java/kr/co/shortenurlservice/domain/ShortenUrlRepository.java
@@ -5,5 +5,6 @@ import java.util.List;
 public interface ShortenUrlRepository {
     void saveShortenUrl(ShortenUrl shortenUrl);
     void asyncSaveShortenUrl(ShortenUrl shortenUrl);
+    void increaseRedirectCount(ShortenUrl shortenUrl);
     ShortenUrl findShortenUrlByShortenUrlKey(String shortenUrlKey);
 }
